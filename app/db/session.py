f22e72d32cdc9ed9bee1b007d063b5d6ff4961e6
Patrick Jones <forksofpower@gmail.com>
2025-05-07 12:15:13 -0500

    add alembic config and setup base-class for models

--- app/db/session.py
+++ app/db/session.py
@@ -25,58 +25,56 @@ AsyncSessionLocal = async_sessionmaker(
     bind=engine, autoflush=False, expire_on_commit=False
 )
 
-Base = declarative_base()
+
+# class DatabaseSessionManager:
+#     _engine: AsyncEngine | None
+#     _sessionmaker: async_sessionmaker | None
+
+#     def __init__(self) -> None:
+#         self._engine = None
+#         self._sessionmaker = None
+
+#     def init(self, host: str):
+#         self._engine = create_async_engine(host)
+#         self._sessionmaker = async_sessionmaker(autocommit=False, bind=self._engine)
+
+#     async def close(self):
+#         if self._engine is None:
+#             raise Exception(DB_EXCEPTION)
+#         await self._engine.dispose()
+#         self._engine = None
+#         self._sessionmaker = None
+
+#     @contextlib.asynccontextmanager
+#     async def connect(self) -> AsyncIterator[AsyncConnection]:
+#         if self._engine is None:
+#             raise Exception(DB_EXCEPTION)
+#         async with self._engine.begin() as connection:
+#             try:
+#                 yield connection
+#             except Exception:
+#                 await connection.rollback()
+#                 raise
+
+#     @contextlib.asynccontextmanager
+#     async def session(self) -> AsyncIterator[AsyncSession]:
+#         if self._sessionmaker is None:
+#             raise Exception(DB_EXCEPTION)
+
+#         session = self._sessionmaker()
+
+#         try:
+#             yield session
+#         except Exception:
+#             await session.rollback()
+#             raise
+#         finally:
+#             await session.close()
 
 
-class DatabaseSessionManager:
-    _engine: AsyncEngine | None
-    _sessionmaker: async_sessionmaker | None
-
-    def __init__(self) -> None:
-        self._engine = None
-        self._sessionmaker = None
-
-    def init(self, host: str):
-        self._engine = create_async_engine(host)
-        self._sessionmaker = async_sessionmaker(autocommit=False, bind=self._engine)
-
-    async def close(self):
-        if self._engine is None:
-            raise Exception(DB_EXCEPTION)
-        await self._engine.dispose()
-        self._engine = None
-        self._sessionmaker = None
-
-    @contextlib.asynccontextmanager
-    async def connect(self) -> AsyncIterator[AsyncConnection]:
-        if self._engine is None:
-            raise Exception(DB_EXCEPTION)
-        async with self._engine.begin() as connection:
-            try:
-                yield connection
-            except Exception:
-                await connection.rollback()
-                raise
-
-    @contextlib.asynccontextmanager
-    async def session(self) -> AsyncIterator[AsyncSession]:
-        if self._sessionmaker is None:
-            raise Exception(DB_EXCEPTION)
-
-        session = self._sessionmaker()
-
-        try:
-            yield session
-        except Exception:
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
+# sessionmanager = DatabaseSessionManager()
 
 
-sessionmanager = DatabaseSessionManager()
-
-
-async def get_db():
-    async with sessionmanager.session() as session:
-        yield session
+# async def get_db():
+#     async with sessionmanager.session() as session:
+#         yield session
--- app/models/base_class.py
+++ app/models/base_class.py
@@ -1,6 +1,7 @@
-from typing import ClassVar
 from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 
 class Base(DeclarativeBase):
-    __tablename__: ClassVar[str] = declared_attr(lambda cls: cls.__name__.lower())  # type: ignore[assignment]
+    @declared_attr  # type: ignore
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()  # type: ignore
