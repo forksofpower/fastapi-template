1717e2566dec49035a62c2468034380fc86c3a42
Patrick Jones <forksofpower@gmail.com>
2025-05-26 12:33:14 -0500

    split auth

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,13 +1,8 @@
-import uuid
 from fastapi_users.authentication import (
     AuthenticationBackend,
     BearerTransport,
     JWTStrategy,
 )
-from fastapi_users import FastAPIUsers
-
-from app.core.user import get_user_manager
-from app.models.user import User
 from app.core.config import settings
 
 
@@ -22,7 +17,3 @@ auth_backend = AuthenticationBackend(
     transport=bearer_transport,
     get_strategy=get_jwt_strategy,
 )
-
-fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
-
-current_active_user = fastapi_users.current_user(active=True)
--- app/core/user.py
+++ app/core/user.py
@@ -1,11 +1,12 @@
 import uuid
 from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, UUIDIDMixin
+from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin
 from fastapi_users.db import SQLAlchemyUserDatabase
 
 from app.api.deps import get_user_db
 from app.core.config import settings
 from app.models.user import User
+from .auth import auth_backend
 
 
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
@@ -30,3 +31,8 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
 
 async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
     yield UserManager(user_db)
+
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
+
+current_active_user = fastapi_users.current_user(active=True)
