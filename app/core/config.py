57236e1dc62b0f386a9f1f65a87673662fa5377e
Patrick Jones <forksofpower@gmail.com>
2025-05-30 12:13:00 -0500

    add entrypoint for environment-based settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,30 @@
+from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from app.core.settings.app import AppSettings
+from app.core.settings.base import AppEnvTypes, BaseAppSettings
+from app.core.settings.development import DevAppSettings
+from app.core.settings.production import ProdAppSettings
+from app.core.settings.test import TestAppSettings
+
+
+AppEnvType = DevAppSettings | TestAppSettings | ProdAppSettings
+
+environments: dict[str, type[AppEnvType]] = {
+    AppEnvTypes.development: DevAppSettings,
+    AppEnvTypes.testing: TestAppSettings,
+    AppEnvTypes.production: ProdAppSettings,
+}
+
+
+@lru_cache
+def get_app_settings() -> AppSettings:
+    """
+    Return application config
+    """
+    app_env = BaseAppSettings.app_env
+    config = environments[app_env]
+    return config()  # type: ignore
 
 
 class Settings(BaseSettings):
